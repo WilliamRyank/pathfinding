fed076b87d1ec5689d5988044104eaec107cdc99
WilliamRyank <kwilliamryan@yahoo.com>
2020-05-30 14:30:12 +0700

    fix naming

--- src/Algorithm/AlgoType.js
+++ src/Algorithm/AlgoType.js
@@ -2,4 +2,4 @@ export const BFS = 'Breadth-first Search';
 export const DFS = 'Depth-first Search';
 export const ASTAR = 'A* Search';
 export const GREEDY = 'Greedy Best-first Search'
-export const BIDIRECTIONAL = "Bi-Directional Search"
+export const BIDIRECTIONAL = "Bi-Directional BFS Search"
